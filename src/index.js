3f1213f966ae0264a98448e727dc69049edae040
ildak-systems <ildak@uw.edu>
2022-12-26 01:58:52 -0800

    Changed project to a music-rec app. Got a working prototype. Focus on building features next.

--- src/index.js
+++ src/index.js
@@ -2,19 +2,32 @@ import axios from 'axios';
 import gsap from 'gsap';
 // do not do the {}, does not work
 
-const API_THREE_HOUR_FIVE_DAYS = "https://api.openweathermap.org/data/2.5/forecast?q=";
-const API_LAT_LONG_HOURLY = ""
+const API_CURRENT_WEATHER_CITY = "https://api.openweathermap.org/data/2.5/weather?q=";
+const API_CURRENT_WEATHER_LAT_LON = "https://api.openweathermap.org/data/2.5/weather?lat="
 const KEY = "<SECRET>";
-export async function main(city, state) {
+export async function main(lon, lat, city) {
+
     // clear data before API call, so they don't stack up
     document.getElementById("title").innerText = "";
-    document.getElementById("dates").innerText = "";
+    document.getElementById("weather").innerText = "";
+    document.getElementById("weather-description").innerText = "";
 
     let result = ""
+
     try
     {
-        let response = await axios.get(API_THREE_HOUR_FIVE_DAYS + city + "," + state + "," + "US" + "&appid=" + KEY);
-        result = await JSON.parse(JSON.stringify(response.data));
+        if (lon && lat)
+        {
+            let response = await axios.get(API_CURRENT_WEATHER_LAT_LON + lat
+                + "&lon=" + lon + "&appid=" + KEY);
+            result = await JSON.parse(JSON.stringify(response.data));
+        }
+        else if (city)
+        {
+            let response = await axios.get(API_CURRENT_WEATHER_CITY + city + "&appid=" + KEY);
+            result = await JSON.parse(JSON.stringify(response.data));
+        }
+
     }
     catch (error)
     {
@@ -22,19 +35,33 @@ export async function main(city, state) {
         document.getElementById("title").innerText = "city not found";
     }
 
-    listCondition(result, state)
+    // watch that video where you can do stuff outside of a try/catch block
+    listCondition(result);
 
 
     //console.log(util.inspect(result, false, null, true));
 }
+export function getUserLocation(){
+    if (navigator.geolocation)
+    {
+        navigator.geolocation.getCurrentPosition((position) => {
+            main(position.coords.longitude, position.coords.latitude);
+            }
+        )
+    }
+    else
+    {
+        return reportError("nothing found");
+    }
 
-export function listCondition(result, state) {
+
+}
+
+export function listCondition(result) {
     if (result)
     {
-        document.getElementById("title").innerText = result.city.name + state;
-        for (let i = 0; i < result.list.length; i++)
-        {
-            document.getElementById("dates").innerText += "\n" + result.list[i].dt_txt;
-        }
+        document.getElementById("title").innerText = result.name;
+        document.getElementById("weather").innerText += "\n" + result.weather[0].main;
+        document.getElementById("weather-description").innerText += "\n" + result.weather[0].description;
     }
 }
