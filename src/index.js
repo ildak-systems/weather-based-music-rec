bef6d1e3de6b796688eb837d577f8248e15e5d3d
ildak-systems <ildak@uw.edu>
2022-12-29 15:43:40 -0800

    Integrated Spotify's authorization flow / Recommendation API
    Initial set up. Get access token from spotify and use the token to call the recommendation API. Gives JSON response of list of tracks

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import axios from 'axios';
-import gsap from 'gsap';
+import querystring from 'query-string';
 // do not do the {}, does not work
 
-const API_CURRENT_WEATHER_CITY = "https://api.openweathermap.org/data/2.5/weather?q=";
-const API_CURRENT_WEATHER_LAT_LON = "https://api.openweathermap.org/data/2.5/weather?lat="
-const KEY = "<SECRET>";
+const myAPI = {
+    CURRENT_WEATHER_CITY : "https://api.openweathermap.org/data/2.5/weather?q=",
+    CURRENT_WEATHER_LAT_LON : "https://api.openweathermap.org/data/2.5/weather?lat=",
+    ACCESS_TOKEN_SPOTIFY : 'https://accounts.spotify.com/api/token',
+    RECOMMENDATION_SPOTIFY : "https://api.spotify.com/v1/recommendations?limit=10&seed_artists=&seed_genres=rock"
+
+}
 
 export async function main(lon, lat, city) {
+    const KEY = "<KEY>";
 
     // clear data before API call, so they don't stack up
     document.getElementById("title").innerText = "";
@@ -14,18 +19,17 @@ export async function main(lon, lat, city) {
     document.getElementById("weather-description").innerText = "";
 
     let result = ""
-
     try
     {
         if (lon != null || lat != null)
         {
-            let response = await axios.get(API_CURRENT_WEATHER_LAT_LON + lat
+            let response = await axios.get(myAPI.CURRENT_WEATHER_LAT_LON + lat
                 + "&lon=" + lon + "&appid=" + KEY);
             result = await JSON.parse(JSON.stringify(response.data));
         }
         else if (city)
         {
-            let response = await axios.get(API_CURRENT_WEATHER_CITY + city + "&appid=" + KEY);
+            let response = await axios.get(myAPI.CURRENT_WEATHER_CITY + city + "&appid=" + KEY);
             result = await JSON.parse(JSON.stringify(response.data));
         }
 
@@ -35,34 +39,31 @@ export async function main(lon, lat, city) {
         console.error(error);
         document.getElementById("title").innerText = "city not found";
     }
-
-    // watch that video where you can do stuff outside of a try/catch block
-    listCondition(result);
-
+    finally
+    {
+        listCondition(result);
+    }
 
     //console.log(util.inspect(result, false, null, true));
 }
-export function getUserLocation()
+export async function getUserLocation()
 {
-    function success(position)
-    {
-        main(position.coords.longitude, position.coords.latitude, null);
-    }
-
-    function error()
-    {
-        document.getElementById('title').innerText = 'Unable to retrieve your location';
-    }
-
     if (navigator.geolocation)
     {
-        navigator.geolocation.getCurrentPosition(success, error);
+        navigator.geolocation.getCurrentPosition( async (position) => {
+                main(position.coords.longitude, position.coords.latitude, null);
+                await getRecommendation(await getAccessToken());
+                //console.log(token);
+            },
+            () =>
+            {
+                document.getElementById('title').innerText = 'Unable to retrieve your location'
+            })
     }
     else
     {
         document.getElementById("title").innerText = "Geolocation is not supported by your browser";
     }
-
 
 }
 
@@ -74,3 +75,59 @@ export function listCondition(result) {
         document.getElementById("weather-description").innerText += "\n" + result.weather[0].description;
     }
 }
+
+export async function getRecommendation(token)
+{
+    const recommendation = async () => {
+        try {
+            const response = await axios.get(myAPI.RECOMMENDATION_SPOTIFY,{
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json",
+                    "Authorization" : `Bearer ${token}`,
+                }
+            });
+
+            console.log(response.data);
+            return response.data;
+        }
+        catch (error)
+        {
+            console.log("Error occured while retrieving recommendations: " + error);
+        }
+    }
+    return recommendation();
+}
+
+export async function getAccessToken() {
+    const client_id = "<ID>";
+    const client_secret = "<SECRET>";
+    const auth_token = btoa(client_id + ':' + client_secret)
+
+    const getToken = async () => {
+        try {
+            //make post request to SPOTIFY API for access token, sending relavent info
+            const data = querystring.stringify({grant_type: 'client_credentials'});
+            // data: stuff that gets added on as =x&
+            const response = await axios.post(myAPI.ACCESS_TOKEN_SPOTIFY, data, {
+                headers: {
+                    'Authorization': `Basic ${auth_token}`,
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            })
+
+            console.log(response.data.access_token);
+            return response.data.access_token; // returned to getAuth, which is a function name
+
+        } catch (error) {
+            console.log("Error occurred while retrieving access token: " + error);
+        }
+    };
+
+    return getToken();
+    // if return getToken: save the return value as const, and call the const as a method => const token = await getAccessToken(); await token();
+    // because technically, I am returning a function, not a value, so when it returns, I can execute it as a function.
+    // if return getToken(): The function is already executed and is returning a value. I can do => const token = await getAccessToken();
+    // and token will be a value, not a function
+}
+
