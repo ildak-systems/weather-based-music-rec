b26bf25400981e91f98919ee4325e25497011215
ildak-systems <ildak@uw.edu>
2023-01-02 01:47:22 -0800

    server-side development finished.
    /api/open-weather/get-weather/lon-lat
    /api/open-weather/get-weather/city
    /api/spotify/get-access-token
    /api/spotify/get-tracks
    
    All of them are able to communicate with client-side
    
    Hid API keys by using dotenv

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express();
 const bodyParser = require('body-parser');
 const got = require('got');
+require('dotenv').config();
 
 const myAPI = {
     CURRENT_WEATHER_CITY : "https://api.openweathermap.org/data/2.5/weather?q=",
@@ -10,11 +11,6 @@ const myAPI = {
     ACCESS_TOKEN_SPOTIFY : 'https://accounts.spotify.com/api/token',
     RECOMMENDATION_SPOTIFY : "https://api.spotify.com/v1/recommendations?limit=10&seed_artists=&seed_genres=rock"
 }
-
-const myKEY = {
-    OPENWEATHER_SECRET : <SECRET></SECRET>
-}
-
 // parse application/json
 app.use(bodyParser.json())
 
@@ -22,14 +18,47 @@ app.use(bodyParser.json())
 app.listen(3000, () => console.log("listen at 3000"));
 app.use(express.static('public')); // anything in this directory is public
 
-// List of API requests and response handling
-app.post('/api', async (req, res) => {
-    console.log(req.body);
-
-    let response = await got.get(myAPI.CURRENT_WEATHER_LAT_LON + req.body.lat + "&lon=" + req.body.lon + "&appid=" + myKEY.OPENWEATHER_SECRET);
-    res.send(response.body);
+// /api/open-weather/get-weather/lon-lat
+app.post('/api/open-weather/get-weather/lon-lat', async (local_req, local_res) => {
+    const openweather_response = await got.get(myAPI.CURRENT_WEATHER_LAT_LON + local_req.body.lat + "&lon=" + local_req.body.lon + "&appid=" + process.env.OPENWEATHER_SECRET);
+    local_res.send(openweather_response.body);
 })
 
-// let response = await axios.get(myAPI.CURRENT_WEATHER_LAT_LON + lat
-//                 + "&lon=" + lon + "&appid=" + KEY);
-//             result = await JSON.parse(JSON.stringify(response.data));
+// /api/open-weather/get-weather/city
+app.post('/api/open-weather/get-weather/city', async (local_req, local_res) => {
+    const openweather_response = await got.get(myAPI.CURRENT_WEATHER_CITY + local_req.body.city + "&appid=" + process.env.OPENWEATHER_SECRET);
+    local_res.send(openweather_response.body);
+})
+
+// /api/spotify/get-access-token
+app.get('/api/spotify/get-access-token', async (local_req, local_res) => {
+    const auth_token = btoa(process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET);
+    const options = {
+        headers: {
+            'Authorization': `Basic ${auth_token}`,
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        form: ({
+            grant_type: 'client_credentials'
+        })
+
+    }
+    const spotify_access_token = await got.post(myAPI.ACCESS_TOKEN_SPOTIFY, options);
+    local_res.send(JSON.parse(spotify_access_token.body));
+})
+
+// /api/spotify/get-tracks
+app.post('/api/spotify/get-tracks', async (local_req, local_res) => {
+    const options = {
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Authorization" : `Bearer ${local_req.body.token}`,
+            // make sure to specify .body for response and request
+        }
+    }
+    const spotify_tracks = await got.get(myAPI.RECOMMENDATION_SPOTIFY, options);
+    local_res.send(JSON.parse(spotify_tracks.body));
+})
+
+
--- src/index.js
+++ src/index.js
@@ -1,38 +1,78 @@
-import axios from 'axios';
+import axios from "axios";
 
-export async function getUserLocation()
+export async function onClickUserLocation()
 {
-    if (navigator.geolocation)
-    {
-        navigator.geolocation.getCurrentPosition( async (position) => {
+    const user_position = await getUserLocation();
 
-                const body = {
-                    lon: position.coords.longitude,
-                    lat: position.coords.latitude
-                }
-                const option = {
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-
-                }
-
-                const response = await axios.post('/api',body, option);
-                const result = await response;
-                console.log(result);
-
-            },
-            () =>
-            {
-                document.getElementById('title').innerText = 'Unable to retrieve your location'
-            })
+    const body = {
+        lon: user_position.coords.longitude,
+        lat: user_position.coords.latitude
     }
-    else
-    {
-        document.getElementById("title").innerText = "Geolocation is not supported by your browser";
+    const option = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
     }
+
+    // API call to /api/open-weather/get-weather/lon-lat
+    const weather_response = await axios.post('/api/open-weather/get-weather/lon-lat', body, option);
+    const weather_result = weather_response.data;
+    console.log(weather_result);
+
+    const tracks = await getTracks(null, null, null);
+
+
 
 }
+
+export async function onClickCity(city)
+{
+    const body = {
+        city: city
+    }
+    const options = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+    const weather_response = await axios.post('/api/open-weather/get-weather/city', body, options);
+    const weather_result_body = weather_response.data;
+    //console.log(weather_result);
+
+}
+
+export async function getTracks(weather, age, limit)
+{
+
+    const token_response = await axios.get('/api/spotify/get-access-token');
+    const token = token_response.data.access_token;
+    // make sure to always specify .data / .body. Always, responses come in a big json file where it includes other stuff
+    // other than what I res.send()
+    const data = {
+        token : token,
+    }
+
+    const tracks = await axios.post('/api/spotify/get-tracks', data);
+    console.log(tracks.data);
+}
+
+export function getUserLocation()
+{
+    return new Promise((res, rej) => {
+        if (navigator.geolocation)
+        {
+            navigator.geolocation.getCurrentPosition(res, rej);
+        }
+        else
+        {
+            document.getElementById("title").innerText = "Geolocation is not supported by your browser. Get tracks" +
+                "using city name instead.";
+        }
+
+    });
+}
+
+
 
 
 
